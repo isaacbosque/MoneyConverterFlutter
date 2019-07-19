67957e3720c06f614e60fcf59c92ea845195ea36
isaacbosque <isaacbosque@gmail.com>
2019-07-19 12:03:10 -0300

    Few UI but no logic

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() async {
   runApp(MaterialApp(
     title: "Conversor de Moedas",
     home: Home(),
+    theme: ThemeData(hintColor: Colors.yellow, primaryColor: Colors.white),
   ));
 }
 
@@ -42,36 +43,66 @@ class _HomeState extends State<Home> {
               case ConnectionState.none:
               case ConnectionState.waiting:
                 return Center(
-                    child:
-                    Text(
-                      "Carregando dados",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    )
-                );
+                    child: Text(
+                  "Carregando dados",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ));
               default:
-                if(snapshot.hasError){
+                if (snapshot.hasError) {
                   return Center(
-                      child:
-                      Text(
-                        "Erro ao carregar dados",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      )
-                  );
-                }else{
-                  return Container(
-                    child:
-                    Center(
-                        child:
-                        Text(
-                          "Dados Carregados",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        )
-                    ),
-                  );
+                      child: Text(
+                    "Erro ao carregar dados",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ));
+                } else {
+                  return App(Dolar,Euro,Real);
                 }
-
             }
           }),
+    );
+  }
+
+  Widget App(Double dolar) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Divider(),
+          Icon(
+            Icons.monetization_on,
+            color: Colors.yellow,
+            size: 150,
+          ),
+          Divider(),
+          textArea("Real", "R\$"),
+          Divider(),
+          textArea("Dolar", "USD"),
+          Divider(),
+          textArea("Euro", "€"),
+        ],
+      ),
+    );
+  }
+
+  Widget textArea(String label, String prefix) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+      child: TextField(
+        keyboardType: TextInputType.numberWithOptions(),
+        decoration: InputDecoration(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.yellow)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.yellow)),
+          prefixText: prefix,
+          prefixStyle: TextStyle(color: Colors.yellow),
+          border: OutlineInputBorder(),
+          labelText: label,
+          labelStyle: TextStyle(fontSize: 20, color: Colors.yellow),
+        ),
+        style: TextStyle(color: Colors.yellow),
+      ),
     );
   }
 }
